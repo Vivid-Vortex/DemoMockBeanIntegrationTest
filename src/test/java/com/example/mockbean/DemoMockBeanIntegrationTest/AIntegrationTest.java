a18787384f15f22a265f6e90ad2f949d7e25f6b6
Vivid-Vortex <ctntc9.dpk@gmail.com>
2025-05-21 15:13:13 +0530

    fetch values from yml file

--- src/test/java/com/example/mockbean/DemoMockBeanIntegrationTest/AIntegrationTest.java
+++ src/test/java/com/example/mockbean/DemoMockBeanIntegrationTest/AIntegrationTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
@@ -13,6 +14,7 @@ import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @Import(TestConfig.class)
+@ActiveProfiles("test")
 class AIntegrationTest {
 
 	@Autowired
